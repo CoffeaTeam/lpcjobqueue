94c364e5c05b06391aed7ed161cf3686c5419654
Nick Smith <nick.smith@cern.ch>
2021-03-03 14:15:02 -0600

    Improve logging and try harder to close things properly

--- src/lpcjobqueue/cluster.py
+++ src/lpcjobqueue/cluster.py
@@ -56,8 +56,6 @@ class LPCCondorJob(HTCondorJob):
                 raise ValueError(
                     f"log_directory must be a subpath of {homedir} or else the schedd cannot write our logs back to the container"
                 )
-            self.job_header_dict.pop("Stream_Output")
-            self.job_header_dict.pop("Stream_Error")
 
         self.job_header_dict.update(
             {
@@ -86,10 +84,9 @@ class LPCCondorJob(HTCondorJob):
 
     async def start(self):
         """ Start workers and point them to our local scheduler """
-        logger.debug("Starting worker: %s", self.name)
+        logger.info("Starting worker: %s", self.name)
 
         job = self.job_script()
-        logger.debug(job)
         job = htcondor.Submit(job)
 
         def sub():
@@ -98,7 +95,7 @@ class LPCCondorJob(HTCondorJob):
                 with SCHEDD.transaction() as txn:
                     cluster_id = job.queue(txn, ad_results=classads)
 
-                logger.debug(classads)
+                logger.debug(f"ClassAds for job {cluster_id}: {classads}")
                 SCHEDD.spool(classads)
                 return cluster_id
             except htcondor.HTCondorInternalError as ex:
@@ -110,45 +107,57 @@ class LPCCondorJob(HTCondorJob):
             self.known_jobs.add(self.job_id)
             weakref.finalize(self, self._close_job, self.job_id)
 
-            logger.debug("Starting job: %s", self.job_id)
-            # await super().start() all this does is set a flag
+            logger.info("Starting job: %s", self.job_id)
+            # dask_jobqueue Job class does some things we don't want
+            # so we do what is done in distributed.ProcessInterface
             self.status = Status.running
 
     async def close(self):
-        logger.debug("Closing worker: %s job: %s", self.name, self.job_id)
+        logger.info(f"Closing worker {self.name} job_id {self.job_id} (current status: {self.status})")
         if self._cluster:
             # workaround for https://github.com/dask/distributed/issues/4532
-            ret = await self._cluster().scheduler_comm.retire_workers(names=[self.name])
-            logger.debug(f"Worker retirement info: {ret}")
+            ret = await self._cluster().scheduler_comm.retire_workers(names=[self.name], remove=True, close_workers=True)
+            # adaptive cluster scaling seems to call this properly already, so may be a no-op
+            logger.debug(f"Worker {self.name} retirement info: {ret}")
 
         def check_gone():
             return len(SCHEDD.query(f"ClusterId == {self.job_id}")) == 0
 
-        for _ in range(10):
+        for _ in range(30):
             await asyncio.sleep(1)
             if await asyncio.get_event_loop().run_in_executor(None, check_gone):
+                logger.info(f"Gracefully closed job {self.job_id}")
                 self.known_jobs.discard(self.job_id)
+                self.status = Status.closed
+                self._event_finished.set()
                 return
 
-        logger.debug(
+        logger.info(
             "Reached timeout, forcefully stopping worker: %s job: %s",
             self.name,
             self.job_id,
         )
 
         def stop():
-            return SCHEDD.act(htcondor.JobAction.Remove, f"ClusterId == {self.job_id}")
+            res = SCHEDD.act(htcondor.JobAction.Remove, f"ClusterId == {self.job_id}")
+            if res["TotalSuccess"] == 1 and res["TotalChangedAds"] == 1:
+                return True
+            return False
 
         result = await asyncio.get_event_loop().run_in_executor(None, stop)
-        logger.debug(f"Closed job {self.job_id}, result {result}")
-        self.known_jobs.discard(self.job_id)
+        if result:
+            logger.info(f"Forcefully closed job {self.job_id}")
+            self.known_jobs.discard(self.job_id)
+            self.status = Status.closed
+            self._event_finished.set()
+            return
+        logger.error(f"Failed to forcefully close job {self.job_id}")
 
     @classmethod
     def _close_job(cls, job_id):
         if job_id in cls.known_jobs:
-            logger.info(f"Closeing job {job_id} in a finalizer")
-            result = SCHEDD.act(htcondor.JobAction.Remove, f"ClusterId == {job_id}")
-            logger.debug(f"Closed job {job_id}, result {result}")
+            logger.warning(f"Last-ditch attempt to close HTCondor job {job_id} in finalizer! You should confirm the job exits!")
+            SCHEDD.act(htcondor.JobAction.Remove, f"ClusterId == {job_id}")
             cls.known_jobs.discard(job_id)
 
 
