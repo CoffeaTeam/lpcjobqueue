b413ef5771193d4735e019bc712c34d3ca058bd1
Nick Smith <nick.smith@cern.ch>
2021-03-04 09:15:50 -0600

    Escape boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,14 @@ cat <<EOF > shell
 #!/usr/bin/env bash
 
 
-if [ "$1" != "" ]; then
+if [ "\$1" != "" ]; then
   export COFFEA_IMAGE=coffeateam/coffea-dask:latest
 else
-  export COFFEA_IMAGE=$1
+  export COFFEA_IMAGE=\$1
 fi
 
 singularity exec -B \${PWD}:/srv -B /uscmst1b_scratch --pwd /srv \\
-  /cvmfs/unpacked.cern.ch/registry.hub.docker.com/${COFFEA_IMAGE} \\
+  /cvmfs/unpacked.cern.ch/registry.hub.docker.com/\${COFFEA_IMAGE} \\
   /bin/bash --rcfile /srv/.bashrc
 EOF
 
