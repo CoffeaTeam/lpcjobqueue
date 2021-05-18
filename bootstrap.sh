5a7bf45ed8ba16832602c0461e7af9a5a7b6d28b
Nicholas Smith <nick.smith@cern.ch>
2021-05-18 11:29:20 -0500

    Properly install kernelspec on initialization

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,18 +21,22 @@ install_env() {
   echo "Installing shallow virtual environment in \$PWD/.env..."
   python -m venv --without-pip --system-site-packages .env
   unlink .env/lib64  # HTCondor can't transfer symlink to directory and it appears optional
+  # work around issues copying CVMFS xattr when copying to tmpdir
+  export TMPDIR=\$(mktemp -d -p .)
+  .env/bin/python -m ipykernel install --user
+  rm -rf \$TMPDIR && unset TMPDIR
   .env/bin/python -m pip install -q git+https://github.com/CoffeaTeam/lpcjobqueue.git@v0.2.3
   echo "done."
 }
-
-[[ -d .env ]] || install_env
-source .env/bin/activate
-alias pip="python -m pip"
 
 export JUPYTER_PATH=/srv/.jupyter
 export JUPYTER_RUNTIME_DIR=/srv/.local/share/jupyter/runtime
 export JUPYTER_DATA_DIR=/srv/.local/share/jupyter
 export IPYTHONDIR=/srv/.ipython
+
+[[ -d .env ]] || install_env
+source .env/bin/activate
+alias pip="python -m pip"
 EOF
 
 chmod u+x shell .bashrc
