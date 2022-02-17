bce1d7710254d89700ff611d3ee3c85518e20a07
Nicholas Smith <nick.smith@cern.ch>
2022-02-17 13:41:19 -0600

    Bind-mount cvmfs

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ else
   export COFFEA_IMAGE=\$1
 fi
 
-singularity exec -B \${PWD}:/srv -B /uscmst1b_scratch --pwd /srv \\
+singularity exec -B \${PWD}:/srv -B /cvmfs -B /uscmst1b_scratch --pwd /srv \\
   /cvmfs/unpacked.cern.ch/registry.hub.docker.com/\${COFFEA_IMAGE} \\
   /bin/bash --rcfile /srv/.bashrc
 EOF
