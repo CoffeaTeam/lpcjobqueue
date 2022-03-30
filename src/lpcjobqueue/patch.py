17e33c91d2cf78d67aeea7fbc061af3825b322a9
Angus Hollands <goosey15@gmail.com>
2022-03-30 13:34:40 +0100

    Fix: use `nbytes` for Awkward Arrays

--- src/lpcjobqueue/patch.py
+++ src/lpcjobqueue/patch.py
@@ -7,6 +7,10 @@ from dask.sizeof import sizeof
 
 
 @sizeof.register(awkward.highlevel.Array)
+def sizeof_uproot_generic(obj):
+    return obj.nbytes
+
+
 @sizeof.register(uproot.model.Model)
 def sizeof_uproot_generic(obj):
     return obj.num_bytes
