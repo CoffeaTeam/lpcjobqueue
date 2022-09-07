93d6e4b1417ffeb9bf8b28d033b6d606c1cd2f56
Nick Smith <nick.smith@cern.ch>
2022-09-07 16:27:49 -0500

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,7 +25,7 @@ install_env() {
   export TMPDIR=\$(mktemp -d -p .)
   .env/bin/python -m ipykernel install --user
   rm -rf \$TMPDIR && unset TMPDIR
-  .env/bin/python -m pip install -q git+https://github.com/CoffeaTeam/lpcjobqueue.git@v0.2.5
+  .env/bin/python -m pip install -q git+https://github.com/CoffeaTeam/lpcjobqueue.git@v0.2.6
   echo "done."
 }
 
